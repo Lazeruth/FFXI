c79b5be652dbbc081d91ec6da5f7c2caade355e9
Lazeruth <152437190+Lazeruth@users.noreply.github.com>
2025-04-08 13:02:23 -0700

    Add files via upload

---
+++ sql_starter_gear.sql
@@ -0,0 +1,101 @@
+----------------------------------------------------------------------------------
+-- Agent Attire, Starlet Attire
+----------------------------------------------------------------------------------
+-- These are "hidden effects" until I edit the DATs
+-- 1/18/25 Using Xi Tinkerer I am able to modify the dats and use xipivot to load
+----------------------------------------------------------------------------------
+-- Agent Attire, Starlet Attire Stats
+-- DEF:58 HP+10 STR+5 DEX+5 VIT+5
+-- AGI+5 INT+5 MND+5 CHR+5 Haste+26%
+-- Accuracy+5 Ranged Accurarcy+5 Magic Accuracy+5
+-- Attack+5 Ranged Attack+5 Magic Att. Bonus+5
+-- Critical Hit Rate+1% Magic Critical Hit Rate+1%
+-- Triple Attack+1%
+----------------------------------------------------------------------------------
+
+-- Agent | Starlet (Head)
+INSERT INTO item_mods VALUES (25606,1,12);      -- DEF:12
+INSERT INTO item_mods VALUES (25606,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (25606,25,3);      -- Accuracy +3 
+INSERT INTO item_mods VALUES (25606,26,3);      -- Ranged Accuracy +3 
+INSERT INTO item_mods VALUES (25606,30,3);      -- Magic Accuracy +3 
+
+INSERT INTO item_mods VALUES (25607,1,12);      -- DEF:12
+INSERT INTO item_mods VALUES (25607,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (25607,25,3);      -- Accuracy +3 
+INSERT INTO item_mods VALUES (25607,26,3);      -- Ranged Accuracy +3 
+INSERT INTO item_mods VALUES (25607,30,3);      -- Magic Accuracy +3
+
+-- Agent | Starlet (Body)
+INSERT INTO item_mods VALUES (26974,1,20);      -- DEF:20
+INSERT INTO item_mods VALUES (26974,384,600);   -- Haste +6%
+INSERT INTO item_mods VALUES (26974,2,10);      -- HP +10
+INSERT INTO item_mods VALUES (26974,8,3);       -- STR +3
+INSERT INTO item_mods VALUES (26974,9,3);       -- DEX +3
+INSERT INTO item_mods VALUES (26974,10,3);      -- VIT +3
+INSERT INTO item_mods VALUES (26974,11,3);      -- AGI +3
+INSERT INTO item_mods VALUES (26974,12,3);      -- INT +3
+INSERT INTO item_mods VALUES (26974,13,3);      -- MND +3
+INSERT INTO item_mods VALUES (26974,14,5);      -- CHR +5
+INSERT INTO item_mods VALUES (26974,302,1);     -- Triple Attack 1%
+
+INSERT INTO item_mods VALUES (26975,1,20);      -- DEF:20
+INSERT INTO item_mods VALUES (26975,384,600);   -- Haste +6%
+INSERT INTO item_mods VALUES (26975,2,10);      -- HP +10
+INSERT INTO item_mods VALUES (26975,8,3);       -- STR +3
+INSERT INTO item_mods VALUES (26975,9,3);       -- DEX +3
+INSERT INTO item_mods VALUES (26975,10,3);      -- VIT +3
+INSERT INTO item_mods VALUES (26975,11,3);      -- AGI +3
+INSERT INTO item_mods VALUES (26975,12,3);      -- INT +3
+INSERT INTO item_mods VALUES (26975,13,3);      -- MND +3
+INSERT INTO item_mods VALUES (26975,14,5);      -- CHR +5
+INSERT INTO item_mods VALUES (26975,302,1);     -- Triple Attack 1%
+
+-- Agent | Starlet (Hands)
+INSERT INTO item_mods VALUES (27111,1,9);       -- DEF:9
+INSERT INTO item_mods VALUES (27111,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (27111,8,2);       -- STR +2
+INSERT INTO item_mods VALUES (27111,9,2);       -- DEX +2
+INSERT INTO item_mods VALUES (27111,25,2);      -- Accuracy +2
+INSERT INTO item_mods VALUES (27111,26,2);      -- Ranged Accuracy +2
+INSERT INTO item_mods VALUES (27111,30,2);      -- Magic Accuracy +2 
+
+INSERT INTO item_mods VALUES (27112,1,9);       -- DEF:9
+INSERT INTO item_mods VALUES (27112,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (27112,8,2);       -- STR +2
+INSERT INTO item_mods VALUES (27112,9,2);       -- DEX +2
+INSERT INTO item_mods VALUES (27112,25,2);      -- Accuracy +2
+INSERT INTO item_mods VALUES (27112,26,2);      -- Ranged Accuracy +2
+INSERT INTO item_mods VALUES (27112,30,2);      -- Magic Accuracy +2
+
+-- Agent | Starlet (Legs)
+INSERT INTO item_mods VALUES (27296,1,12);      -- DEF:12
+INSERT INTO item_mods VALUES (27296,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (27296,10,2);      -- VIT +2
+INSERT INTO item_mods VALUES (27296,11,2);      -- AGI +2
+INSERT INTO item_mods VALUES (27296,165,1);     -- Critical Hit Rate +1%
+INSERT INTO item_mods VALUES (27296,562,1);     -- Magic Critical Hit Rate +1% 
+
+INSERT INTO item_mods VALUES (27297,1,12);      -- DEF:12
+INSERT INTO item_mods VALUES (27297,384,500);   -- Haste +5%
+INSERT INTO item_mods VALUES (27297,10,2);      -- VIT +2
+INSERT INTO item_mods VALUES (27297,11,2);      -- AGI +2
+INSERT INTO item_mods VALUES (27297,165,1);     -- Critical Hit Rate +1%
+INSERT INTO item_mods VALUES (27297,562,1);     -- Magic Critical Hit Rate +1%
+
+-- Agent | Starlet (Feet)
+INSERT INTO item_mods VALUES (27467,1,5);       -- DEF:5
+INSERT INTO item_mods VALUES (27467,384,300);   -- Haste +3%
+INSERT INTO item_mods VALUES (27467,12,2);      -- INT +2
+INSERT INTO item_mods VALUES (27467,13,2);      -- MND +2
+INSERT into item_mods VALUES (27467,23,5);      -- Attack +5
+INSERT INTO item_mods VALUES (27467,24,5);      -- Ranged Attack +5
+INSERT INTO item_mods VALUES (27467,28,5);      -- Magic Attack Bonus +5
+
+INSERT INTO item_mods VALUES (27468,1,5);       -- DEF:5
+INSERT INTO item_mods VALUES (27468,384,300);   -- Haste +3%
+INSERT INTO item_mods VALUES (27468,12,2);      -- INT +2
+INSERT INTO item_mods VALUES (27468,13,2);      -- MND +2
+INSERT into item_mods VALUES (27468,23,5);      -- Attack +5
+INSERT INTO item_mods VALUES (27468,24,5);      -- Ranged Attack +5
+INSERT INTO item_mods VALUES (27468,28,5);      -- Magic Attack Bonus +5
